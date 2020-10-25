33d9ff2ea4f63ee8b978fbd28a3950e27f531f6d
Firdaus Alghifari <firdaus.alghifari@gmail.com>
2020-10-25 18:21:15 +0700

    edit fcfs, add mutex lock for resource sharing

--- master.py
+++ master.py
@@ -11,6 +11,8 @@ PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
 if len(sys.argv) >= 2:
     HOST = sys.argv[1]
 
+lock = asyncio.Lock()
+queue = deque()
 WORKER_LIST = [
     {
         "host": os.getenv("WORKER_1_HOST", "127.0.0.1"),
@@ -50,10 +52,9 @@ async def contact_worker(message, host, port):
     return data
 
 async def master_server(reader, writer):
-
-    queue = deque()
-
     data = ""
+    chosen_worker = None
+
     while True:
         MAX_BYTES = 1024
         chunk = await reader.read(MAX_BYTES)  # Max number of bytes to read
@@ -61,32 +62,28 @@ async def master_server(reader, writer):
         if len(chunk) < MAX_BYTES:
             break
     
-    queue.append(data)
+    async with lock:
+        queue.append(data)
     
     while True:
-        if queue[0] == data:
-            chosen_data = queue.popleft()
+        async with lock:
+            if queue[0] == data:
+                for worker in WORKER_LIST:
+                    if worker["conn"] == 0:
+                        worker["conn"] += 1
+                        queue.popleft()
+                        chosen_worker = worker
+                        break
+        if chosen_worker:
             break
         await asyncio.sleep(1) # check queue every 1 second
 
     print(WORKER_LIST)
-
-    chosen_worker = None
-    while True:
-        for worker in WORKER_LIST:
-            if worker["conn"] == 0:
-                worker["conn"] += 1
-                chosen_worker = worker
-                break
-        if chosen_worker:
-            break
-        await asyncio.sleep(10) # check connection every 10 seconds
-        
     port_chosen = chosen_worker["port"]
     mes = "Job handled at %s" % port_chosen
     print(mes)
 
-    processed_data = await contact_worker(chosen_data, chosen_worker["host"], chosen_worker["port"])
+    processed_data = await contact_worker(data, chosen_worker["host"], chosen_worker["port"])
     chosen_worker["conn"] -= 1
 
     notice = "Your job is done by port %s and the result is %s" % (port_chosen, processed_data)
